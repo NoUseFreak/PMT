8da62c248fea6537b5ee7a7a5886c512815a7d78
Joeri van Dooren <joeri.vandooren@gmail.com>
2013-06-04 12:49:57 +0200

    Added comments.

--- src/PMT/CoreBundle/Entity/User.php
+++ src/PMT/CoreBundle/Entity/User.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * This file is part of the PMT package.
+ *
+ * (c) Dries De Peuter <dries@nousefreak.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
 
 namespace PMT\CoreBundle\Entity;
 
