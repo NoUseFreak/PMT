853dd200a229a03e4bab6bef557489607fd5f904
Dries De Peuter <dries@nousefreak.be>
2013-06-08 15:23:13 +0200

    Partial workflow

---
+++ src/PMT/CoreBundle/Entity/Workflow/Workflow.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * This file is part of the PMT package.
+ *
+ * (c) Dries De Peuter <dries@nousefreak.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PMT\CoreBundle\Entity\Workflow;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="workflows", uniqueConstraints={@ORM\UniqueConstraint(name="name_idx", columns={"name"})})
+ * @author Dries De Peuter <dries@nousefreak.be>
+ */
+class Workflow
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     *
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string")
+     * @Assert\NotBlank()
+     *
+     * @var string
+     */
+    private $name;
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $name
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+    }
+
+    /**
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+}
---
+++ src/PMT/CoreBundle/Entity/Workflow/WorkflowStep.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * This file is part of the PMT package.
+ *
+ * (c) Dries De Peuter <dries@nousefreak.be>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PMT\CoreBundle\Entity\Workflow;
+
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
+
+/**
+ * @ORM\Entity
+ * @ORM\Table(name="workflow_steps")
+ * @author Dries De Peuter <dries@nousefreak.be>
+ */
+class WorkflowfStep
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     *
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @Assert\NotBlank()
+     * @ORM\ManyToOne(targetEntity="PMT\CoreBundle\Entity\Workflow\Workflow")
+     * @ORM\JoinColumn(name="workflow_id", referencedColumnName="id")
+     */
+    private $workflow;
+
+    /**
+     * @Assert\NotBlank()
+     * @ORM\ManyToOne(targetEntity="PMT\CoreBundle\Entity\Issue\Status")
+     * @ORM\JoinColumn(name="status_id", referencedColumnName="id")
+     */
+    private $status;
+
+    /**
+     * @param int $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param mixed $status
+     */
+    public function setStatus($status)
+    {
+        $this->status = $status;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getStatus()
+    {
+        return $this->status;
+    }
+
+    /**
+     * @param mixed $workflow
+     */
+    public function setWorkflow($workflow)
+    {
+        $this->workflow = $workflow;
+    }
+
+    /**
+     * @return mixed
+     */
+    public function getWorkflow()
+    {
+        return $this->workflow;
+    }
+}
