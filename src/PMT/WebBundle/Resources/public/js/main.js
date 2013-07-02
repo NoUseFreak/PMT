89f5d5f99f8576635be8ceb19cdab4e050c200d5
Dries De Peuter <dries@nousefreak.be>
2013-07-02 22:03:26 +0200

    Added tooltip to dates

--- src/PMT/WebBundle/Resources/public/js/main.js
+++ src/PMT/WebBundle/Resources/public/js/main.js
@@ -1,5 +1,14 @@
-$(function() {
-    $('time.moment').html(function() {
-        return moment($(this).attr('datetime')).fromNow();
-    })
+$(function () {
+
+    moment.defaultFormat = 'DD-MM-YYYY HH:mm:ss';
+
+    $('time.moment')
+        .html(function () {
+            return moment($(this).attr('datetime')).fromNow();
+        })
+        .tooltip({
+            title: function() {
+                return moment($(this).attr('datetime')).format();
+            }
+        });
 });
