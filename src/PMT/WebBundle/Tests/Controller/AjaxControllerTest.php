ba5c2b68fafc225f20e0d2afd8c339dc7bf22fc2
Dries De Peuter <dries@nousefreak.be>
2013-07-03 22:12:08 +0200

    Add tests for AjaxController

---
+++ src/PMT/WebBundle/Tests/Controller/AjaxControllerTest.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace PMT\WebBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class AjaxControllerTest extends WebTestCase
+{
+    protected $client;
+
+    public function testTagFirewall()
+    {
+        $client = static::createClient();
+        $client->request('POST', '/ajax/tags.json');
+        $this->assertContains('/login', $client->getResponse()->headers->get('location'));
+    }
+
+    public function testTags()
+    {
+        $client = $this->getLoggedInClient();
+        $client->request('POST', '/ajax/tags.json');
+
+        $this->assertTrue(
+            $client->getResponse()->headers->contains(
+                'content-type',
+                'application/json'
+            )
+        );
+
+        $array = (array)json_decode($client->getResponse()->getContent());
+        $this->assertTrue(array_key_exists('tags', $array));
+    }
+
+    public function testMilestones()
+    {
+        $client = $this->getLoggedInClient();
+        $client->request('POST', '/ajax/milestones.json');
+
+        $this->assertTrue(
+            $client->getResponse()->headers->contains(
+                'content-type',
+                'application/json'
+            )
+        );
+
+        $array = (array)json_decode($client->getResponse()->getContent());
+        $this->assertTrue(array_key_exists('milestones', $array));
+    }
+
+    protected function getLoggedInClient()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/login');
+        $form = $crawler->selectButton('_submit')->form(
+            array(
+                '_username' => getenv('PMTWEB_AUTH_USER'),
+                '_password' => getenv('PMTWEB_AUTH_PASSWORD'),
+            )
+        );
+        $client->submit($form);
+        $client->followRedirect();
+
+        return $client;
+    }
+}
